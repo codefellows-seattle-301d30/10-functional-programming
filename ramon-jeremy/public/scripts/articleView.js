75a7816798ae6942c54a8acda40337413f909d07
Jeremy Crawford <jfcraw08@gmail.com>
2018-02-26 14:30:20 -0800

    not sure if its working but want to save changes after code review

--- ramon-jeremy/public/scripts/articleView.js
+++ ramon-jeremy/public/scripts/articleView.js
@@ -1,7 +1,7 @@
 'use strict';
 var app = app || {};
 
-(module => {
+(function(module) {
 
   var articleView = {};
 
@@ -132,13 +132,13 @@ var app = app || {};
   };
 
   articleView.initAdminPage = () => {
-  
+
   // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
-    // app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(stat.toHtml2()));
+    app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
     $('#blog-stats .articles').text(app.Article.all.length);
     $('#blog-stats .words').text(app.Article.numWordsAll());
   };
   module.articleView = articleView;
-})(app)
+})(app);
